ba906091566e0d8f05a204a0e47d76666a67f061
oti-auditeur <paul.oti.auditeur@lecnam.net>
2023-11-14 11:08:18 -1000

    V.0.0.4  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	deleted:    database/migrations/2023_11_14_181044_create_paddlers_table.php
    	deleted:    database/migrations/2023_11_14_181045_create_trainings_table.php
    	deleted:    database/migrations/2023_11_14_181046_create_trainings_details_table.php
    	deleted:    database/migrations/2023_11_14_181048_create_trainings_team_table.php
    	deleted:    database/migrations/2023_11_14_181049_create_attendances_table.php
    	modified:   database/migrations/2023_11_14_195819_create_paddlers_table.php

--- database/migrations/2023_11_14_195819_create_paddlers_table.php
+++ database/migrations/2023_11_14_195819_create_paddlers_table.php
@@ -14,13 +14,22 @@ return new class extends Migration
         Schema::disableForeignKeyConstraints();
 
         Schema::create('paddlers', function (Blueprint $table) {
-            $table->integer('paddlers_id')->primary();
+            $table->increments('paddlers_id'); // Utilisez increments pour déclarer une clé primaire auto-incrémentée
             $table->string('name', 255)->nullable();
             $table->string('first_name', 255)->nullable();
             $table->date('birthday')->nullable();
             $table->string('mail_address', 255)->nullable();
             $table->string('password', 255)->nullable();
             $table->integer('experience_level')->nullable();
+        });
+
+        Schema::create('cultural_elements', function (Blueprint $table) {
+            $table->increments('cultural_elements_id');
+            $table->unsignedInteger('paddlers_id')->index()->nullable();
+            $table->foreign('paddlers_id')->references('paddlers_id')->on('paddlers');
+            $table->string('title', 255)->nullable();
+            $table->string('description', 255)->nullable();
+            $table->string('type_video_article_event', 255)->nullable();
         });
 
         Schema::enableForeignKeyConstraints();
@@ -31,6 +40,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('cultural_elements');
         Schema::dropIfExists('paddlers');
     }
 };
+
